8e7f55d661d0be31accb24e44bed4c32b202439c
Pongsathorn Kerdphol <aqover@hotmail.com>
2017-03-12 02:10:22 +0700

    Fix startup
    fix app.py - start service on system boot

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import scan
 from src.udp_socket import Udp
@@ -11,39 +12,46 @@ class BLE(object):
 		self.mac_address = ''
 		self.status = 0
 
-		os.system("sudo hciconfig hci" + str(hci) + " > tmp")
+		os.system("sudo hciconfig hci" + str(hci) + " > /var/tmp/locating")
 
-		fd = open("tmp", 'r')
+		fd = open("/var/tmp/locating", 'r')
 		lines = fd.readlines()
 		fd.close()
 
-		self.mac_address = lines[1].split()[2]
-		self.status = (lines[2].strip() == "UP RUNNING")
-		os.system("rm tmp")
+		if len(lines) > 5:
+			self.mac_address = lines[1].split()[2]
+			self.status = (lines[2].strip() == "UP RUNNING")
+		os.system("rm /var/tmp/locating")
 
 	def GetMac_address(self):
 		return self.mac_address
 
 	def GetStatus(self):
-		os.system("sudo hciconfig hci" + str(self.hci) + " > tmp")
+		os.system("sudo hciconfig hci" + str(self.hci) + " > /var/tmp/locating")
 
-		fd = open("tmp", 'r')
+		fd = open("/var/tmp/locating", 'r')
 		lines = fd.readlines()
 		fd.close()
 
-		self.status = (lines[2].strip() == "UP RUNNING")
-		os.system("rm tmp")
+		if len(lines) > 5:
+			self.status = (lines[2].strip() == "UP RUNNING")
+		else:
+			self.status = False
+		os.system("rm /var/tmp/locating")
 
 		return self.status
 		
 
 if __name__ == '__main__':
+	time.sleep(5)
+
 	device = BLE(0)
 
 	print (device.GetMac_address())
 	print (device.GetStatus())
 
-	scan.MAC_ADDRESS = device.GetMac_address()
+	if device.GetMac_address() != '':
+		scan.MAC_ADDRESS = device.GetMac_address()
 	try:
 		ip = Udp().GetServer()
 		if ip != None:
